a58030cc1b36c8a3f50b406ed1e6f25d157ecff2
Teemu Viitanen <92275334+tviitanen@users.noreply.github.com>
2022-03-27 14:41:55 +0300

    returned version

--- src/project/HTPerusKirjasto.c
+++ src/project/HTPerusKirjasto.c
@@ -2,7 +2,7 @@
 /* CT60A2500 C-ohjelmoinnin perusteet
  * Tekijä: Teemu Viitanen
  * Opiskelijanumero: 00456573
- * Päivämäärä: 1.3.2022
+ * Päivämäärä: 26.3.2022
  * Palauttamalla tämän työn arvioitavaksi vakuutan, että
  * 1) Olen itse kirjoittanut kaiken tässä tiedostossa olevan koodin
  * 2) En ole antanut tätä koodia kenenkään muun käyttöön
@@ -10,7 +10,7 @@
  * Kurssin oppimateriaalien lisäksi työhön ovat vaikuttaneet seuraavat
  * lähteet ja henkilöt, ja se näkyy koodissa seuraavissa kohdissa:
  * - Stackoverflow: memset & struct tm
- * - 
+ * -
  */
 /*************************************************************************/
 /* Tavoitetaso, HTPerusKirjasto.c */
@@ -23,29 +23,37 @@
 #include <time.h>
 #include "HTPerusKirjasto.h"
 
-LISTA *lueTiedosto(LISTA *pAlku, char *tiedostoNimi){
-    char aRivi[3*MAXLEN];
+LISTA *lueTiedosto(LISTA *pAlku, char *tiedostoNimi)
+{
+    char aRivi[3 * MAXLEN];
+    struct tm tm; // Tietue ajan käsittelyyn
 
     FILE *tiedosto;
-    
-    if ((tiedosto = fopen(tiedostoNimi, "r")) == NULL){
+
+    if ((tiedosto = fopen(tiedostoNimi, "r")) == NULL)
+    {
         perror("Tiedoston avaaminen epäonnistui, lopetetaan");
         exit(0);
     }
 
     LISTA *pUusi;
-    fgets(aRivi, 3*MAXLEN, tiedosto); // Otsikkorivi pois
+    fgets(aRivi, 3 * MAXLEN, tiedosto); // Otsikkorivi pois
 
-    while(fgets(aRivi, 3*MAXLEN, tiedosto) != NULL){ // luetaan tiedosto rivi kerrallaan
-        if ((pUusi = (LISTA *)malloc(sizeof(LISTA))) == NULL){ // varataan muisti
+    while (fgets(aRivi, 3 * MAXLEN, tiedosto) != NULL)
+    { // luetaan tiedosto rivi kerrallaan
+        if ((pUusi = (LISTA *)malloc(sizeof(LISTA))) == NULL)
+        { // varataan muisti
             perror("Muistinvaraus epäonnistui.");
             printf("\n");
             exit(0);
         }
-        
+
         // erotellaan tiedot riviltä ja lisätään structiin
         char *aika = strtok(aRivi, ";");
-        strcpy(pUusi->pvm, aika);
+        // printf("%s\t", aika);
+        strptime(aika, "%d.%m.%Y %H:%M", &tm);
+        pUusi->pvm = tm;
+
         pUusi->viikko = atoi(strtok(NULL, ";"));
         pUusi->kulutus = atoi(strtok(NULL, ";"));
         pUusi->aurinko = atoi(strtok(NULL, ";"));
@@ -57,14 +65,16 @@ LISTA *lueTiedosto(LISTA *pAlku, char *tiedostoNimi){
         pUusi->pSeuraava = NULL;
 
         // lisätään struct linkitettyyn listaan
-        if (pAlku == NULL){
+        if (pAlku == NULL)
+        {
             pAlku = pUusi;
         }
-        else{
+        else
+        {
             LISTA *pTemp = pAlku;
             while (pTemp->pSeuraava != NULL)
                 pTemp = pTemp->pSeuraava;
-                pTemp->pSeuraava = pUusi;
+            pTemp->pSeuraava = pUusi;
         }
     }
     fclose(tiedosto);
@@ -73,72 +83,80 @@ LISTA *lueTiedosto(LISTA *pAlku, char *tiedostoNimi){
     return (pAlku);
 }
 
-
-TULOKSET analysoiTiedot(LISTA *pAlku){
+TULOKSET analysoiTiedot(LISTA *pAlku)
+{
     TULOKSET tulos;
     int i = 0;
     long int kokonaisKulutus = 0;
-    char aikaMax[MAXLEN], aikaMin[MAXLEN];
     LISTA *pTemp = pAlku;
 
-    int min = 10000000, max = 0;
+    // Alustetaan ensimmäisillä arvoilla
+    int min = pTemp->kulutus, max = pTemp->kulutus;
+    struct tm aikaMax = pTemp->pvm, aikaMin = pTemp->pvm;
+
     double ka = 0;
 
     // käydään linkitetty lista läpi
-    while (pTemp != NULL){
-        if ((pTemp->kulutus) > max){ // selvitetään suurin kulutus & aika
+    while (pTemp != NULL)
+    {
+        if ((pTemp->kulutus) > max)
+        { // selvitetään suurin kulutus & aika
             max = pTemp->kulutus;
-            strcpy(aikaMax, pTemp->pvm);            
+            aikaMax = pTemp->pvm;
         }
-        if ((pTemp->kulutus) < min){ // selvitetään pienin kulutus & aika
+        if ((pTemp->kulutus) < min)
+        { // selvitetään pienin kulutus & aika
             min = pTemp->kulutus;
-            strcpy(aikaMin, pTemp->pvm);
+            aikaMin = pTemp->pvm;
         }
         i++;
         kokonaisKulutus = kokonaisKulutus + pTemp->kulutus;
         pTemp = pTemp->pSeuraava;
     }
 
-    ka = (double)kokonaisKulutus/i; // typecast, että saa desimaalit
+    ka = (double)kokonaisKulutus / i; // typecast, että saa desimaalit
     // lisätään saadut tiedot structiin
     tulos.lkm = i;
     tulos.kulutus = kokonaisKulutus;
     tulos.keskiarvo = ka;
     tulos.maxKulutus = max;
-    strcpy(tulos.maxAika, aikaMax);
+    tulos.maxAika = aikaMax;
     tulos.minKulutus = min;
-    strcpy(tulos.minAika, aikaMin);
+    tulos.minAika = aikaMin;
+
     printf("Analysoitu %d mittaustulosta.\n", i);
     printf("Kokonaiskulutus oli yhteensä %ld kWh.\n", kokonaisKulutus);
-    
-    return(tulos);
+
+    return (tulos);
 }
 
-
-KKTUOTANTO *kuukausiAnalyysi(LISTA *pAlku){
+KKTUOTANTO *kuukausiAnalyysi(LISTA *pAlku)
+{
     long int kuukausiTuotanto = 0;
     char kk[5];
-    int i=0;
+    int i = 0;
     KKTUOTANTO *pTaulukko; // Taulukko kk tuotannoille
-    struct tm tm; // Tietue ajan käsittelyyn
     LISTA *pTemp = pAlku;
     // Varataan muisti 12 structille
     // huom! calloc alustaa arvot nolliksi
-    if ((pTaulukko = (KKTUOTANTO *)calloc(12, sizeof(KKTUOTANTO))) == NULL){
+    if ((pTaulukko = (KKTUOTANTO *)calloc(12, sizeof(KKTUOTANTO))) == NULL)
+    {
         perror("Muistinvaraus epäonnistui.");
         printf("\n");
         exit(0);
     }
 
-    while (pTemp != NULL){ // käydään linkitetty lista läpi
+    // käydään linkitetty lista läpi
+    while (pTemp != NULL)
+    {
         // Tunnistetaan kuukausi kk
-        memset(&tm, 0, sizeof(tm));
-        strptime(pTemp->pvm, "%d.%m.%y %H:%M:%S", &tm);
-        strftime(kk, sizeof(kk), "%m", &tm);
+        strftime(kk, sizeof(kk), "%m", &pTemp->pvm);
 
-        for(i = 0; i<12; i++) { // lasketaan kuukausittaiset tuotannot ja lisätään taulukkoon
-            pTaulukko[i].kuukausi = i+1; // kuukausi numerot
-            if (atoi(kk) == i+1){
+        for (i = 0; i < 12; i++)
+        {                                  // lasketaan kuukausittaiset tuotannot ja lisätään taulukkoon
+            pTaulukko[i].kuukausi = i + 1; // kuukausi numerot
+            if (atoi(kk) == i + 1)
+            {
                 kuukausiTuotanto = pTaulukko[i].tuotanto + pTemp->yht + pTemp->aurinko + pTemp->errLampo + pTemp->tuuli + pTemp->vesi + pTemp->ydin;
                 pTaulukko[i].kuukausi = atoi(kk);
                 pTaulukko[i].tuotanto = kuukausiTuotanto;
@@ -149,34 +167,41 @@ KKTUOTANTO *kuukausiAnalyysi(LISTA *pAlku){
 
     printf("Kuukausittaiset tuotannot analysoitu.\n");
     return pTaulukko;
-    }
+}
 
-
-void tallennaTiedot(TULOKSET tulos, KKTUOTANTO aTaulukko[], char *tiedostoNimi){
+void tallennaTiedot(TULOKSET tulos, KKTUOTANTO aTaulukko[], char *tiedostoNimi)
+{
+    // käsitellään struct tm
+    char min[MAXLEN], max[MAXLEN];
+    strftime(max, sizeof(max), "%d.%m.%Y %H:%M", &tulos.maxAika);
+    strftime(min, sizeof(min), "%d.%m.%Y %H:%M", &tulos.minAika);
 
     // TULOSTETAAN NÄYTÖLLE
     printf("Tilastotiedot %d mittaustuloksesta:\n", tulos.lkm);
     printf("Kulutus oli yhteensä %ld kWh, ja keskimäärin %.1f kWh.\n", tulos.kulutus, tulos.keskiarvo);
-    printf("Suurin kulutus, %d kWh, tapahtui %s.\n", tulos.maxKulutus, tulos.maxAika);
-    printf("Pienin kulutus, %d kWh, tapahtui %s.\n\n", tulos.minKulutus, tulos.minAika);
+    printf("Suurin kulutus, %d kWh, tapahtui %s.\n", tulos.maxKulutus, max);
+    printf("Pienin kulutus, %d kWh, tapahtui %s.\n\n", tulos.minKulutus, min);
     printf("Pvm;Tuotanto (GWh)\n");
-    for (int i=0; i<12;i++) {
+    for (int i = 0; i < 12; i++)
+    {
         printf("Kk %d;%.2lf\n", aTaulukko[i].kuukausi, (float)aTaulukko[i].tuotanto / 1000000);
     }
 
     FILE *tiedosto;
-    if ((tiedosto = fopen(tiedostoNimi, "w")) == NULL){
+    if ((tiedosto = fopen(tiedostoNimi, "w")) == NULL)
+    {
         perror("Tiedoston avaaminen epäonnistui, lopetetaan");
         exit(0);
-    }        
+    }
     // TULOSTETAAN TIEDOSTOON
     fprintf(tiedosto, "Tilastotiedot %d mittaustuloksesta:\n", tulos.lkm);
     fprintf(tiedosto, "Kulutus oli yhteensä %ld kWh, ja keskimäärin %.1f kWh.\n", tulos.kulutus, tulos.keskiarvo);
-    fprintf(tiedosto, "Suurin kulutus, %d kWh, tapahtui %s.\n", tulos.maxKulutus, tulos.maxAika);
-    fprintf(tiedosto, "Pienin kulutus, %d kWh, tapahtui %s.\n\n", tulos.minKulutus, tulos.minAika);
+    fprintf(tiedosto, "Suurin kulutus, %d kWh, tapahtui %s.\n", tulos.maxKulutus, max);
+    fprintf(tiedosto, "Pienin kulutus, %d kWh, tapahtui %s.\n\n", tulos.minKulutus, min);
     fprintf(tiedosto, "Pvm;Tuotanto (GWh)\n");
 
-    for (int i=0; i<12;i++) { // tulostetaan kk analyysi
+    for (int i = 0; i < 12; i++)
+    { // tulostetaan kk analyysi
         fprintf(tiedosto, "Kk %d;%.2lf\n", aTaulukko[i].kuukausi, (float)aTaulukko[i].tuotanto / 1000000);
     }
 
@@ -186,10 +211,11 @@ void tallennaTiedot(TULOKSET tulos, KKTUOTANTO aTaulukko[], char *tiedostoNimi){
     return;
 }
 
-
-LISTA *vapautaMuisti(LISTA *pAlku){
+LISTA *vapautaMuisti(LISTA *pAlku)
+{
     LISTA *ptr = pAlku;
-    while (ptr != NULL){
+    while (ptr != NULL)
+    {
         pAlku = ptr->pSeuraava;
         free(ptr);
         ptr = pAlku;
